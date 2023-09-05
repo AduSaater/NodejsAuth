e8901cc9b432c3d4ad6901e015e8c53909160c58
AduSaater <66807679+AduSaater@users.noreply.github.com>
2023-09-05 20:14:59 +0100

    error message

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,12 @@ const jwt = require('jsonwebtoken');
 module.exports = function(req, res, next) {
     const token = req.headers.authorization;
     if (!token || !token.startsWith('Bearer ')) {
-        return res.status(401).json({message:'Not authorized to access this route.'});
+        return res.status(401).json({
+            success:false,
+            error:{
+                message:'Not authorized to access this route.'
+            }
+        });
     }
     const tokenValue = token.split(' ')[1]; // Extract token value
 
